1e7b7822b7c4ff18774f9e56b93a1abec3aaccf9
Akshay KM <akshay194@gmail.com>
2023-10-05 12:58:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import Contact from "./components/Contact";
+import SectionOne from "./components/SectionOne";
+import Who from "./components/Who";
+import Works from "./components/Works";
+import SectionTwo from "./components/SectionTwo";
+import SectionThree from "./components/SectionThree";
+import Footer from "./components/footer";
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
+function App() {
+  return (
+    <Container>
+      <SectionOne />
+      <SectionTwo/>
+      <SectionThree/>
+      <Footer/> 
+      {/* <Who />
+      <Works />
+      <Contact /> */}
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/SectionTwo.jsx
@@ -0,0 +1,160 @@
+
+import React, { Suspense } from "react";
+import styled from "styled-components";
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  @media only screen and (max-width: 768px) {
+    height: 200vh;
+  }
+`;
+
+const Container = styled.div`
+  height: 100%;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const Left = styled.div`
+  flex: 3;
+  position: relative;
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    width: 100%;
+  }
+  
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+
+  @media only screen and (max-width: 768px) {
+    text-align: center;
+  }
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    text-align: center;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    align-items: center;
+  }
+`;
+
+const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+ 
+
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+    height: 300px;
+  }
+
+  @keyframes animate {
+    to {
+      transform: translateY(20px);
+    }
+  }
+`;
+function SectionTwo() {
+  return (
+    <Section>
+
+      <Container>
+        <Left>
+         {/* <Canvas>
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <ambientLight intensity={1} />
+              <directionalLight position={[3, 2, 1]} />
+              <Sphere args={[1, 100, 200]} scale={2.4}>
+                <MeshDistortMaterial
+                  color="#3d1c56"
+                  attach="material"
+                  distort={0.5}
+                  speed={2}
+                />
+              </Sphere>
+            </Suspense>
+          </Canvas> */}
+          <Img src="./img/cap3.png" />
+        </Left>
+        <Right>
+        <Title>Next-Gen Tech</Title>
+          <WhatWeDo>
+            {/* <Line src="./img/line.png" /> */}
+            <Subtitle>What we Do</Subtitle>
+          </WhatWeDo>
+          <Desc>
+          NeoSynaptics isn't just a cap. It's a leap into tomorrow, embedding cutting-edge features seamlessly into everyday wear.
+          </Desc>
+          {/* <Button>Learn More</Button> */}
+          
+        </Right>
+      </Container>
+    </Section>
+  )
+}
+
+export default SectionTwo
