5935071959b043356f1bd4c78d0ee368c13aa93d
Unnikrishnan R <68398724+unni2000@users.noreply.github.com>
2023-10-06 17:15:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import SectionOne from "./components/SectionOne";
 import SectionTwo from "./components/SectionTwo";
 import SectionThree from "./components/SectionThree";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 const Container = styled.div`
   height: 100vh;
