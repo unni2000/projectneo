b06edd0f079040ee64aa387d353fdd395015639d
Akshay KM <akshay194@gmail.com>
2023-10-11 16:05:24 +0530

    barchart added to components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,52 +2,9 @@
 import React, { Suspense } from "react";
 import styled from "styled-components";
 import StationNav from "../components/StationNav";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import BarChartOne from "../components/BarChartOne";
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+
 const DashboardContainer = styled.div`
   height: 100vh;
   scroll-snap-type: y mandatory;
@@ -98,6 +55,9 @@ const Right = styled.div`
 background-color: blueviolet;
   flex: 2;
   position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   @media only screen and (max-width: 768px) {
     flex: 1;
     width: 100%;
@@ -138,32 +98,12 @@ function Dashboard() {
     <Container>
         <Left>
           <LeftSection>
-        <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
-        </BarChart>
-      </ResponsiveContainer>
+         <BarChartOne/>
       </LeftSection>
         </Left>
         <Right>
           <RightSection>
-
+         
           </RightSection>
         </Right>
       </Container>
